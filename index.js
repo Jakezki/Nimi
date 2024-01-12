e40fc15f0bf987591817acdef43c38eb35622ef2
Jakezki <145100021+Jakezki@users.noreply.github.com>
2024-01-12 10:04:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let tervehdys = "Hei, ";
 let hymio = "🙂";
 let omatervehdys = tervehdys + omanimi;
 
-
+String.prototype.lisaaHymio = function() {
+  return this + hymio;
+};
 
 console.log(omatervehdys.lisaaHymio());
 console.log(omatervehdys.lisaaHymio().lisaaHymio());
